082e0455c24f78dbcfd62d9d3fb7d42a0952f6f8
Evelyn Ll <lligui97@students.rowan.edu>
2023-05-05 11:00:50 -0400

    part 1 completed

--- 1. Button Interrupts/Part1.c
+++ 1. Button Interrupts/Part1.c
@@ -2,7 +2,7 @@
  * Part1.c
  *
  *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
+ *      Author: eve
  *
  *  This code is a template which will change the color of the LED being blinked using the interrupt routine.
  */
@@ -66,7 +66,7 @@ void gpioInit(){
       // Configure Button on P2.3 as input with pullup resistor
       P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
       P2REN |= BIT3;                          // P2.3 pull-up register enable
-      P2IES &= ~BIT3;                         // P2.3 Low --> High edge
+      P2IES |= BIT3;                          // P2.3 High --> Low edge
       P2IE |= BIT3;                           // P2.3 interrupt enabled
 
 }
@@ -78,18 +78,22 @@ void gpioInit(){
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-    P2IFG &= ~BIT3;                         // Clear P1.3 IFG
+    P2IFG &= ~BIT3;                         // Clear P2.3 IFG
 
-    if ( )       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
+    if (P2IES & BIT3)       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
     {
         LED_Color = 0;
+        P1OUT &= ~BIT0;
         // @TODO Add code to change which edge the interrupt should be looking for next
+        P2IES &= ~BIT3;
     }
 
-    else if ( ) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
+    else if (!(P2IES & BIT3))// @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
     {
         LED_Color = 1;
+        P6OUT &= ~BIT6;
         // @TODO Add code to change which edge the interrupt should be looking for next
+        P2IES |= BIT3;
     }
 }
 
